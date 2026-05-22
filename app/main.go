eed305cab78f9a3efa9a635a4bbca32f5cb3df41
Kevin De Coninck <kevin.dconinck@gmail.com>
2026-05-22 08:42:11 +0200

    Initialize: Go
    - Initialize the 'app/' folder as a Go module.
    - Add the main entry point for the Spot CLI.

---
+++ app/main.go
@@ -0,0 +1,12 @@
+// =====================================================================================================================
+// == LICENSE:                 Copyright (c) 2026 Kevin De Coninck.
+// == SPDX-License-Identifier: LicenseRef-PolyForm-Noncommercial-1.0.0
+// =====================================================================================================================
+
+// Package main implements Spot CLI, a pattern-matching, static analysis engine.
+package main
+
+// The main entry point for the Spot CLI.
+func main() {
+	// NOTE: Intentionally left blank.
+}
