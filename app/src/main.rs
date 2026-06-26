601d452a6c56cc711f9244cf4c18d7c69d62a55e
Kevin De Coninck <kevin.dconinck@gmail.com>
2026-06-26 13:40:52 +0200

    Setup: Rust
    - Initialize the 'app/' folder as a Rust binary crate.
    - Setup VS Code for Rust development.
    - Update '.gitignore': Prevent the 'target/' folder from being committed
      to the GIT repository.

---
+++ app/src/main.rs
@@ -0,0 +1,11 @@
+// =====================================================================================================================
+// == LICENSE:                 Copyright (c) 2026 Kevin De Coninck.
+// == SPDX-License-Identifier: LicenseRef-PolyForm-Noncommercial-1.0.0
+// =====================================================================================================================
+//! Re-Spot, or rSpot is a highly configurable, static language analyzer.
+
+/// The main entry point for the application.
+/// This function is executed when the application is started.
+fn main() {
+    todo!() // NOTE: Implement this function.
+}
